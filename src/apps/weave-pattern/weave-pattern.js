4e1d3ca315623da60053da8b2d5d913687501ef3
alex <alex@eg312.com>
2014-10-16 13:57:05 +0300

    added weave pattern

---
+++ src/apps/weave-pattern/weave-pattern.js
@@ -0,0 +1,138 @@
+var imageData;
+var width;
+var height;
+var c;
+var weaveW = 0;
+var weaveH = 0;
+var weavePattern;
+var topSize = 0;
+var topPattern = [];
+var middlePattern = [];
+var leftSize = 0;
+var leftPattern = [];
+
+function createImageData(canvasId) {
+	var element = document.getElementById(canvasId);
+	c = element.getContext("2d");
+
+	element.width = $(document).width();
+	element.height = $(document).height();
+	
+	width = element.width;
+	height = element.height;
+
+	imageData = c.createImageData(1000, 1000);
+	
+	weaveH = 300;
+	weaveW = 300;
+	topSize = 6;
+	leftSize = 6;
+	
+	topPattern = new Array(weaveH);
+	for (var i = 0; i < weaveW; i++) {
+		topPattern[i] = new Array(weaveW);
+	}
+	
+	middlePattern = new Array(weaveH);
+	for (var i = 0; i < weaveH; i++) {
+		middlePattern[i] = new Array(weaveW);
+	}
+	
+	leftPattern = new Array(weaveH);
+	for (var i = 0; i < weaveH; i++) {
+		leftPattern[i] = new Array(weaveW);
+	}
+	
+	weavePattern = new Array(weaveH);
+	for (var i = 0; i < weaveH; i++) {
+		weavePattern[i] = new Array(weaveW);
+	}
+	
+	initPatterns();
+	anim();
+	
+	c.putImageData(imageData, 0, 0);
+}
+
+function initPatterns() {
+	for (var i = 0; i < weaveH; i++) {
+		for (var j = 0; j < weaveW; j++) {
+			if (i < topSize) {
+				topPattern[i][j] = randInt(0, 1);
+			} else {
+				topPattern[i][j] = 0;
+			}
+			
+			if (j < leftSize) {
+				leftPattern[i][j] = randInt(0, 1);
+			} else {
+				leftPattern[i][j] = 0;
+			}
+			
+			if (i < topSize && j < leftSize) {
+				middlePattern[i][j] = randInt(0, 1);
+			} else {
+				middlePattern[i][j] = 0;
+			}
+		}
+	}
+}
+
+function multiplyMatrices(first, second) {
+    var newMatrix = [],
+        newWidth = second[0].length,
+        newHeight = first.length;
+    //iterating through first matrix rows
+    for (var row = 0; row < newHeight; row++) {
+        newMatrix[row] = [];
+        //iterating through second matrix columns
+        for (var column = 0; column < newWidth; column++) { 
+            var sum = 0;
+            //calculating sum of pairwise products
+            for (var index = 0; index < first[0].length; index++) {
+                sum += first[row][index] * second[index][column];
+            }
+            newMatrix[row][column] = sum;
+        }
+    }
+    return newMatrix;
+}
+
+function createPattern() {
+	var tmp = multiplyMatrices(leftPattern, middlePattern);
+	var result = multiplyMatrices(tmp, topPattern);
+	weavePattern = result.slice(0);
+}
+
+function anim() {
+	createPattern();
+	draw(imageData);
+	c.putImageData(imageData, 0, 0);
+}
+
+function draw(imageData) {
+	
+	var rc1 = randInt(1, 255);
+	var rc2 = randInt(1, 255);
+	var rc3 = randInt(1, 255);
+	
+	for (var x = 0; x < weaveW; x++) {
+		for (var y = 0; y < weaveH; y++) {
+			index = x + y * weaveW;
+			
+			var rr = parseInt(weavePattern[y][x]) * rc1 % 255;
+			var gg = parseInt(weavePattern[y][x]) * rc1 % 255;
+			var bb = parseInt(weavePattern[y][x]) * rc1 % 255;
+			
+			setPixel(imageData, x, y, rr, gg, bb, 255);
+		}
+	}
+}
+
+function setPixel(imageData, x, y, r, g, b, a) {
+    index = (x + y * imageData.width) * 4;
+    imageData.data[index+0] = r;
+    imageData.data[index+1] = g;
+    imageData.data[index+2] = b;
+    imageData.data[index+3] = a;
+}
