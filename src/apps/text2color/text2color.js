ccb18a6ec13629d44967f6f418e31780ec1d4237
alex <alex@eg312.com>
2014-10-21 21:08:34 +0300

    misc changes to text 2 color

--- src/apps/text2color/text2color.js
+++ src/apps/text2color/text2color.js
@@ -40,19 +40,25 @@ function anim() {
 }
 
 function draw(imageData) {
-	var stepH = 5;
-	var stepW = 5;
+	var stepH = 15;
+	var stepW = 15;
 	
-	for (var x = 0; x < width; x+=stepW) {
-		for (var y = 0; y < height; y+=stepH) {
-			var index = (x) + (y) * width;
-			var r = parseInt(pattern[index]);
-			var g = parseInt(pattern[index]);
-			var b = parseInt(pattern[index]);
+	for (var y = 0; y < height/stepH; y++) {
+		for (var x = 0; x < width/stepW; x++) {
+			var index = x + y * width;
 			
-			for (var x2 = 0; x2 < stepW; x2+=1) {
-				for (var y2 = 0; y2 < stepH; y2+=1) {
-					setPixel(imageData, x + x2, y + y2, r, g, b, 255);
+			if (index > pattern.length)
+				break;
+			
+			var rgb = hsvToRgb(((pattern[index] * pattern[index] * 13) % 255) * 360/255, 1, 1);
+			
+			var r = Math.round(rgb[0]);//parseInt(pattern[index]);
+			var g = Math.round(rgb[1]);//parseInt(pattern[index]) * Math.random();
+			var b = Math.round(rgb[2]);//parseInt(pattern[index]) * Math.random();
+			
+			for (var x2 = 0; x2 < stepW; x2++) {
+				for (var y2 = 0; y2 < stepH; y2++) {
+					setPixel(imageData, x * stepW + x2, y * stepH + y2, r, g, b, 255);
 				}
 			}
 		}
@@ -66,3 +72,21 @@ function setPixel(imageData, x, y, r, g, b, a) {
     imageData.data[index+2] = b;
     imageData.data[index+3] = a;
 }
+
+function hsvToRgb(h, s, v) {  
+    var c = v * s;  
+    var h1 = h / 60;  
+    var x = c * (1 - Math.abs((h1 % 2) - 1));  
+    var m = v - c;  
+    var rgb = [0, 0, 0];  
+      
+    if (typeof h == 'undefined') rgb = [0, 0, 0];  
+    else if (h1 < 1) rgb = [c, x, 0];  
+    else if (h1 < 2) rgb = [x, c, 0];  
+    else if (h1 < 3) rgb = [0, c, x];  
+    else if (h1 < 4) rgb = [0, x, c];  
+    else if (h1 < 5) rgb = [x, 0, c];  
+    else if (h1 <= 6) rgb = [c, 0, x];  
+      
+    return [255 * (rgb[0] + m), 255 * (rgb[1] + m), 255 * (rgb[2] + m)];  
+  }   
